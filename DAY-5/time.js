729c8974d75cce21897d5e930faab8e4920fb003
Mukesh <vprlks20@gmail.com>
2025-12-09 09:00:01 +0530

    day 5

---
+++ DAY-5/time.js
@@ -0,0 +1,25 @@
+// console.log("first");
+// setTimeout(() => {
+//     console.log("This run after");
+// }, 5000);
+// console.log("end") 
+
+// setTimeout(() => {
+//   console.log("1");
+//   setTimeout(() => {
+//     console.log("2");
+//     setTim/eout(() => {
+//       console.log("3");
+//       setTimeout(() => {
+//         console.log("4");
+//         setTimeout(() => {
+//           console.log("5");
+//         }, 1000);
+//       }, 2000);
+//     }, 3000);
+//   }, 4000);
+// }, 5000);
+
+
+
+
